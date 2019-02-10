952e0e2bc1a010156d0271f6d13890737cb0ee6b
Siloah Audie Catalon <s.a.catalon@gmail.com>
2019-02-10 20:53:54 +0800

    Initial Commit

---
+++ src/components/CounterForm.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class CounterForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.resetState = {
+            title: '',
+            isValid: true,
+        };
+        this.state = this.resetState;
+    }
+
+    setForm = event => {
+        const {value} = event.target;
+
+        this.setState({
+            title: value,
+            isValid: true,
+        });
+    }
+
+    submitForm = () => {
+        const { title } = this.state;
+
+        if ( title.trim().length ) {
+            this.props.addCounter(this.state.title);
+            this.setState(this.resetState);
+        } else {
+            this.setState({
+               isValid: false,
+            });
+        }
+    }
+
+    render() {
+        const { title } = this.state;
+
+        const buttonStyle = {
+            maxHeight: '38px',
+        };
+
+        let inputClass = this.state.isValid ? 'form-control' : 'form-control is-invalid';
+
+        return (
+            <div className="row my-1 justify-content-between">
+                <div className="col-12 p-0">
+                    <small className="form-text text-muted">Add a counter.</small>
+                </div>
+                <div className="col-10 p-0">
+                    <input value={title} onChange={this.setForm} className={inputClass} placeholder="Title" type="text" />
+                    <div className="invalid-feedback">
+                        Provide a title!
+                    </div>
+                </div>
+                <button onClick={this.submitForm} className="btn btn-outline-dark col-2" style={buttonStyle}>
+                    <i className="fas fa-plus"></i>
+                </button>
+            </div>
+        );
+    }
+}
+
+export default CounterForm;
