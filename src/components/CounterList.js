83b582f3579f8354923c84cf056fdd24d68af7b3
Siloah Audie Catalon <s.a.catalon@gmail.com>
2019-02-10 21:43:25 +0800

    Add tooltip

--- src/components/CounterList.js
+++ src/components/CounterList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 class CounterList extends React.Component {
     constructor(props) {
@@ -20,18 +21,20 @@ class CounterList extends React.Component {
                 <li key={index} className="list-group-item justify-content-between align-items-center d-flex">
                     <h5 className="m-0">{listItem.title}</h5>
                     <div className="btn-group">
-                        <button onClick={() => this.props.incrementCounter(listItem.id)} className="btn btn-increment btn-outline-secondary">
+                        <button onClick={() => this.props.incrementCounter(listItem.id)} className="btn btn-increment btn-outline-dark">
                             <i className="fas fa-plus"></i>
                         </button>
                         <button className="btn itemCount btn-outline-secondary" style={style.buttonMinWidth} disabled>
                             {listItem.count}
                         </button>
-                        <button onClick={() => this.props.decrementCounter(listItem.id)} className="btn btn-decrement btn-outline-secondary">
+                        <button onClick={() => this.props.decrementCounter(listItem.id)} className="btn btn-decrement btn-outline-dark">
                             <i className="fas fa-minus"></i>
                         </button>
-                        <button onClick={() => this.props.deleteCounter(listItem.id)} className="btn btn-delete btn-outline-danger">
-                            <i className="fas fa-trash"></i>
-                        </button>
+                        <OverlayTrigger placement="top" overlay={ <Tooltip>Delete {listItem.title} <i className="fas fa-question"></i></Tooltip> }>
+                            <button onClick={() => this.props.deleteCounter(listItem.id)} className="btn btn-delete btn-outline-danger">
+                                <i className="fas fa-trash"></i>
+                            </button>
+                        </OverlayTrigger>
                     </div>
                 </li>
             );
